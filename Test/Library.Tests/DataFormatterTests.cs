4569b6448d1f925fbb7847bb32b68acf01283f88
santiagopereyraecheverria <santy812pe@gmail.com>
2024-04-17 19:06:26 -0300

    Hasta el paso 5

---
+++ Test/Library.Tests/DataFormatterTests.cs
@@ -0,0 +1,15 @@
+namespace Library.Tests;
+
+public class DateFormatterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
