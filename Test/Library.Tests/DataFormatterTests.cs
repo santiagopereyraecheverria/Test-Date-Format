fa3eb274979771aec7b63b153c4d6c48b21aa031
santiagopereyraecheverria <santy812pe@gmail.com>
2024-04-17 19:27:24 -0300

    Hasta el paso 6

--- Test/Library.Tests/DataFormatterTests.cs
+++ Test/Library.Tests/DataFormatterTests.cs
@@ -1,15 +1,51 @@
-namespace Library.Tests;
-
-public class DateFormatterTests
+namespace Library.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class DateFormatterTests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+            // Configuración común para los casos de prueba
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [Test]
+        // Una fecha en formato correcto
+        public void ChangeFormat_CorrectFormat_ReturnsExpectedFormat()
+        {
+            // Arrange
+            string inputDate = "17/04/2024";
+            string expectedOutput = "2024-04-17";
+
+            // Act
+            string actualOutput = Ucu.Poo.TestDateFormat.DateFormatter.ChangeFormat(inputDate);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [Test]
+        //Una fecha que no tenga el formato correcto
+        public void ChangeFormat_IncorrectFormat_ReturnsInvalidOutput()
+        {
+            // Arrange
+            string inputDate = "2024-04-17"; // Formato incorrecto
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => Ucu.Poo.TestDateFormat.DateFormatter.ChangeFormat(inputDate));
+        }
+
+        [Test]
+        // Una Fecha en Blanco
+        public void ChangeFormat_BlankInput_ReturnsEmptyString()
+        {
+            // Arrange
+            string inputDate = "";
+
+            // Act
+            string actualOutput = Ucu.Poo.TestDateFormat.DateFormatter.ChangeFormat(inputDate);
+
+            // Assert
+            Assert.AreEqual(string.Empty, actualOutput);
+        }
     }
 }
